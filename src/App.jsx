ed651f16fc107eb0598d1233a99b367db48a300c
lussolli <leonardo.lussolli@gmail.com>
2024-10-14 20:25:42 -0300

    Finalizando tela inicial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
     <div className="App">
-      <h1>Secret Word</h1>
+      <StartScreen />
     </div>
   );
 }
