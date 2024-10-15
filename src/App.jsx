d4fabe2c4b28fbcc85636535414a18824d9a9744
lussolli <leonardo.lussolli@gmail.com>
2024-10-14 21:39:29 -0300

    Finalizando a tela de fim de jogo.

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver retry={retry} />}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
